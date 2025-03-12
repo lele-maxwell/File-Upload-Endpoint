daf24419f6dfc9e58154b7ed502e1aa72f5e0ca2
lele-maxwell <leleivanlele22@gmail.com>
2025-03-12 08:35:25 +0100

    updated to file upload endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,56 @@
-use axum::{routing::get, Router};
+use axum::{
+    routing::get,
+    response::Html,
+    extract::Multipart,
+    Router
+};
+use std::{
+    fs::File,
+    io::Write
+};
+
+async fn index() -> Html<&'static str> {
+    Html(std::include_str!("../public/index.html"))
+}
+
+async fn upload(mut multipart: Multipart) {
+    while let Some(field) = multipart
+        .next_field().await.expect("Failed to get next field!")
+    {
+        if field.name().unwrap() != "fileupload" {
+            continue;
+        }
+        
+        // Grab the name
+        let file_name = field.file_name()
+            .unwrap();
+
+        // Create a path for the soon-to-be file
+        let file_path = format!("files/{}", file_name);
+        
+        // Unwrap the incoming bytes
+        let data = field.bytes()
+            .await
+            .unwrap();
+
+        // Open a handle to the file
+        let mut file_handle = File::create(file_path)
+            .expect("Failed to open file handle!");
+
+        // Write the incoming data to the handle
+        file_handle.write_all(&data)
+            .expect("Failed to write data!");
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    let addr = "127.0.0.1:7777";
+    let app = Router::new()
+        .route("/", get(index).post(upload));
 
-    let response: tokio::net::TcpListener = tokio::net::TcpListener::bind(addr).await.unwrap();
-
-    axum::serve(response, router()).await.unwrap()
-}
-
-fn router() -> Router {
-    Router::new().route("/", get(display))
-}
-
-async fn display() -> &'static str {
-    "Hello, World!"
+    let listener = tokio::net::TcpListener::bind("localhost:3000")
+        .await.expect("Failed to start listener!");
+    
+    axum::serve(listener, app)
+        .await.expect("Failed to serve 'app'!");
 }
